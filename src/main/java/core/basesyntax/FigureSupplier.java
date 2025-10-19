438f59655e5e6facf98719efe8f8c21ac067c0c8
Dmytro <sibertdom.wot@gmail.com>
2025-10-19 12:03:00 +0300

    fix: Resolve checkstyle errors

--- src/main/java/core/basesyntax/FigureSupplier.java
+++ src/main/java/core/basesyntax/FigureSupplier.java
@@ -3,11 +3,11 @@ package core.basesyntax;
 import java.util.Random;
 
 public class FigureSupplier {
-    private final ColorSupplier colorSupplier = new ColorSupplier();
-    private final Random random = new Random();
     private static final int MAX_PROPERTY_VALUE = 10;
     private static final double DEFAULT_RADIUS = 10.0;
     private static final String DEFAULT_COLOR = "white";
+    private final ColorSupplier colorSupplier = new ColorSupplier();
+    private final Random random = new Random();
 
     public Figure getRandomFigure() {
         // Генеруємо випадкове число для вибору типу фігури (0, 1, 2)
